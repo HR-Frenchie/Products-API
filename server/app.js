e85a1de18b5705a8c2d9233a33e8ffc63088b04e
Alex Caron <alex.caron@gmail.com>
2023-05-27 12:56:56 -0500

    set up basic productsId route

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const port = 3000;
-// set up routes
 
 const app = express();
+
+app.get('/products/:productId', (req, res) => {
+  res.send('Hello World!')
+});
 
 app.listen(port, () => console.log("server listening on port: ", port));
