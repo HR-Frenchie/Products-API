691ad4d17720bda147c863e25c383976ca025a32
Alex Caron <alex.caron@gmail.com>
2023-05-30 18:04:28 -0500

    final changes

--- seed.sql
+++ seed.sql
@@ -26,8 +26,6 @@ INSERT INTO related_products (SELECT * FROM temp_related_products WHERE NOT seco
 
 DROP TABLE temp_related_products;
 
-CREATE INDEX products_index ON products (product_id);
-CREATE INDEX styles_index ON styles (style_id);
 CREATE INDEX styles_product_index ON styles (product_id);
 CREATE INDEX skus_index ON skus (style_id);
 CREATE INDEX photos_index ON photos (style_id);
